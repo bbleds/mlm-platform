d366df16fa4b7409820438164f85c63d58af84cf
Benjamin Bledsoe <bledsoedesigns@gmail.com>
2018-04-23 15:10:30 -0400

    Adding edit UI for admin users

--- client/src/containers/AdminUsers.js
+++ client/src/containers/AdminUsers.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { Redirect } from 'react-router-dom'
-import { Avatar } from 'material-ui'
+import { Redirect, Link } from 'react-router-dom'
+import { 
+	Table,
+	TableHeader,
+	TableHeaderColumn,
+	TableBody,
+	TableFooter,
+	TableRow,
+	TableRowColumn,
+	Toolbar,
+	ToolbarGroup,
+	ToolbarTitle,
+	RaisedButton,
+	Paper
+} from 'material-ui'
 import actions from '../actions'
 
 class AdminUsers extends Component{
@@ -29,9 +42,51 @@ class AdminUsers extends Component{
 						user === null ? (<div>Loading application</div>) :
 							(<div>
 									{
-										user.permissions == 'admin' && user.approved ?  
-										<h1>{mockContent}</h1> :
-										<h1>We are currently waiting on your account to be approved. Once it has been approved, you will receive an email and be able to access protected content. Thank you!</h1>
+										user.permissions == 'admin' && user.approved && users ?  
+										(
+											<Paper zDepth={1}>
+												<Toolbar>
+													<ToolbarGroup>
+														<ToolbarTitle text="Manage Users" />
+													</ToolbarGroup>
+												</Toolbar>
+												<Table>
+													<TableHeader>
+														<TableRow>
+															<TableHeaderColumn tooltip="The user's name">Name</TableHeaderColumn>
+															<TableHeaderColumn tooltip="The user's email address">Email</TableHeaderColumn>
+															<TableHeaderColumn tooltip="The user's permissions">Permissions</TableHeaderColumn>
+															<TableHeaderColumn tooltip="The action you wish to apply">Actions</TableHeaderColumn>
+														</TableRow>
+													</TableHeader>
+													<TableBody>
+														{
+															users.map((i) => {
+																return (
+																	<TableRow key={i.id}>
+																		<TableRowColumn>{i.first_name} {i.last_name}</TableRowColumn>
+																		<TableRowColumn>{i.email}</TableRowColumn>
+																		<TableRowColumn>{i.permissions}</TableRowColumn>
+																		<TableRowColumn>
+																			<Link to="/"><RaisedButton primary={true} label="Edit" /></Link>
+																			<Link to="/"><RaisedButton secondary={true} label="Delete" /></Link>
+																		</TableRowColumn>
+																	</TableRow>
+																)
+															})
+														}
+													</TableBody>
+													<TableFooter adjustForCheckbox={false}>
+															<TableRow>
+																<TableRowColumn colSpan="4" style={{textAlign: 'center'}}>
+																	Display Delete Selected button here
+																</TableRowColumn>
+															</TableRow>
+														</TableFooter>
+												</Table>
+											</Paper>
+										) :
+										<div>Loading application</div>
 									}
 							</div>)
 				}
